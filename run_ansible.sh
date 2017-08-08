750fed884079a0a202ec6ab849945e230861f20d
Joseph Mulloy <github@joe.mulloy.me>
2017-08-08 12:07:56 +0000

    Fix python path

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -i 'localhost,' -c local playbook.yml -e 'ansible_python_interpreter=/usr/local/bin/python' -e@vars.yml
+ansible-playbook -i 'localhost,' -c local playbook.yml -e 'ansible_python_interpreter=/usr/local/bin/python2.7' -e@vars.yml
